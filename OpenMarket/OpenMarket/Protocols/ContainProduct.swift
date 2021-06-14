22d9793c82d87d451538316f02fb3c002519914c
innieminnie <rkddlsl1001@gmail.com>
2021-06-14 16:47:44 +0900

    feat: ContainProduct 프로토콜 생성

---
+++ OpenMarket/OpenMarket/Protocols/ContainProduct.swift
@@ -0,0 +1,15 @@
+//
+//  ContainProduct.swift
+//  OpenMarket
+//
+//  Created by 강인희 on 2021/06/14.
+//
+
+protocol ContainProducts {
+    var productList: [Product] { get }
+}
+extension ContainProducts {
+    var productList: [Product] {
+        return OpenMarketAPIManager.shared.productList
+    }
+}
